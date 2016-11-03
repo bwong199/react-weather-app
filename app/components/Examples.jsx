546338c650a504f672383dcefc8f715ad4dac8a0
bwong199 <bwong199@my.bcit.ca>
2016-11-03 09:10:19 -0600

    Added foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Examples = React.createClass({
 // 	render: function(){
 // 		return (
@@ -12,7 +12,22 @@ var React = require('react');
 
 var Examples = (props) => {
 	return (
-		<h3>Examples Component</h3>
+		<div>
+		<h1 className="text-center">Examples Component</h1>
+		<p>Here are a few example locations to try out: </p>
+		<ol>
+			<li>
+				<Link to='/?location=Calgary,Alberta'>Calgary, Alberta</Link>
+			</li>
+			<li>
+				<Link to='/?location=Rio'>Rio, Brazil</Link>
+			</li>
+			<li>
+
+			</li>
+		</ol>
+		</div>
+
 		)
 	
 }
